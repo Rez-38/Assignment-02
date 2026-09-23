7a9c9230b42cd85fc8ebaea856122ad54b19dc82
Leon <136545384+Rez-38@users.noreply.github.com>
2026-09-22 23:30:02 -0700

    grade-assigner.js complete

--- js/04-grade-assigner.js
+++ js/04-grade-assigner.js
@@ -1,1 +1,24 @@
 document.write(`<h2>The “Grade Assigner” Application</h2>`);
+
+let score = prompt("Enter your score to get a grade...", "1 - 100");
+console.log(`score: ${score}`);
+
+switch (true) {
+    case score < 1 || score > 100:
+        alert("Invalid input, must be a number between 1 and 100");
+        location.reload();
+    case score >= 90:
+        console.log("You received an A");
+        break;
+    case score >= 80:
+        console.log("You received a B");
+        break;
+    case score >= 70:
+        console.log("You received a C");
+        break;
+    case score >= 60: 
+        console.log("You received a D");
+        break;
+    default:
+        console.log("You received an F");
+}
